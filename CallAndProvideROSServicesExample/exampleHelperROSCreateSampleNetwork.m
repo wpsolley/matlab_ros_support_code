958cf84325996312b246dcd84be67d4c03805db8
rojas70 <rojas70@gmail.com>
2024-02-23 18:00:34 +0000

    Improve documentation and aesthetics

--- CallAndProvideROSServicesExample/exampleHelperROSCreateSampleNetwork.m
+++ CallAndProvideROSServicesExample/exampleHelperROSCreateSampleNetwork.m
@@ -14,27 +14,36 @@
 % ROS master at 'localhost'. If you are connecting to an external master,
 % you will have to use its IP address or hostname.
 masterHost = 'localhost';
-node_1 = ros.Node('node_1', masterHost);
+node_1 = ros.Node('node_1'); % If masterHost is localHost no need to add
 node_2 = ros.Node('node_2', masterHost);
 node_3 = ros.Node('node_3', masterHost);
 
 % Example data from pre-existing messages
 messageData = load('specialROSMessageData.mat','scan','tf');
 
+%% Pose: 
 % Create a publisher and subscriber for the '/pose' topic
-twistPub = ros.Publisher(node_1,'/pose','geometry_msgs/Twist',...
-    'DataFormat','struct');
-twistPubmsg = rosmessage(twistPub);
-twistSub = ros.Subscriber(node_2,'/pose','geometry_msgs/Twist',...
-    'DataFormat','struct');
 
+% A.1. Node 1 Publisher
+twistPub = ros.Publisher(node_1,'/pose','geometry_msgs/Twist','DataFormat','struct');
+
+% A.2. Every publisher will need to have messages to send
+twistPubmsg = rosmessage(twistPub);
+
+% B.1 Node 1 Subscriber: this node is designed to receive info from node1
+twistSub = ros.Subscriber(node_2,'/pose','geometry_msgs/Twist','DataFormat','struct');
+
+%% Scan
 % Create publishers and subscribers for the '/scan' topic
-scanPub = ros.Publisher(node_3,'/scan','sensor_msgs/LaserScan',...
-    'DataFormat','struct');
-scanSub1 = ros.Subscriber(node_1,'/scan','sensor_msgs/LaserScan',...
-    'DataFormat','struct');
-scanSub2 = ros.Subscriber(node_2,'/scan','sensor_msgs/LaserScan',...
-    'DataFormat','struct');
+
+% C.1. Node 3 Publisher
+scanPub = ros.Publisher(node_3,'/scan','sensor_msgs/LaserScan','DataFormat','struct');
+
+% D.1 2 nodes will subscribe
+scanSub1 = ros.Subscriber(node_1,'/scan','sensor_msgs/LaserScan','DataFormat','struct');
+scanSub2 = ros.Subscriber(node_2,'/scan','sensor_msgs/LaserScan','DataFormat','struct');
+
+%% Services
 
 % Create two service servers for the '/add' and '/reply' services
 srv1 = ros.ServiceServer(node_3,'/add','roscpp_tutorials/TwoInts',...
@@ -42,8 +51,11 @@ srv1 = ros.ServiceServer(node_3,'/add','roscpp_tutorials/TwoInts',...
 srv2 = ros.ServiceServer(node_3,'/reply','std_srvs/Empty',...
     @exampleHelperROSEmptyCallback,'DataFormat','struct');
 
+%% TF
 % Load sample data for inspecting messages
 tf = messageData.tf;
+
+%% Timers
 
 % Create a timer for publishing messages and assign appropriate handles
 % The timer will call exampleHelperROSSimTimer at a rate of 10 Hz.
